3f2cad65c699cb945ba7e15d33cbaa1a864aede5
Elizaveta56 <93541422+Elizaveta56@users.noreply.github.com>
2022-12-19 15:57:40 +0300

    Add files via upload

---
+++ medit/medit/SingInViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class SingInViewController: UIViewController {
+
+    @IBOutlet weak var inputPassword: UITextField!
+    @IBAction func signInAction(_ sender: UIButton) {
+    }
+}
+
